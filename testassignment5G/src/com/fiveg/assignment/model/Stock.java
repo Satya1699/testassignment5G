eb3122be4097a8f71cc09ff200616137d8080bc9
Satya <sri4satya@gmail.com>
2016-12-31 20:45:58 +0530

    Model creation and commit

---
+++ testassignment5G/src/com/fiveg/assignment/model/Stock.java
@@ -0,0 +1,37 @@
+package com.fiveg.assignment.model;
+
+public class Stock implements Comparable<Stock> {
+   String stockLine;
+   double rowPrice;
+   
+public String getStockLine() {
+	return stockLine;
+}
+
+public void setStockLine(String stockLine) {
+	this.stockLine = stockLine;
+}
+
+public double getRowPrice() {
+	return rowPrice;
+}
+
+public void setRowPrice(double rowPrice) {
+	this.rowPrice = rowPrice;
+}
+
+@Override
+public String toString() {
+	return "[ "+stockLine+"    -> "+ rowPrice+" ]"; 
+}
+@Override
+public int compareTo(Stock stockObj) {
+	
+	if(rowPrice > stockObj.getRowPrice())
+		return 1;
+	else if(rowPrice < stockObj.getRowPrice())
+		return -1;
+	else return 0;
+}
+   
+}
